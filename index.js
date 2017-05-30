99c7f27f1d7bfd00c03fa616ac76d6fc1cba5a2a
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2017-05-30 09:48:12 +0200

    Use bindings to load add-on

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 if (process.platform === 'win32') {
-  module.exports = require('./Build/Release/fswin.node');
+  module.exports = require('bindings')('fswin.node');
 } else {
 	throw 'this module only works on windows';
 }
